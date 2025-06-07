6725dd54cdc5e9e3e7d919670090cd00ff5a2543
aman <amanvishwanath@gmail.com>
2025-06-07 12:48:05 -0400

    07/06/2025

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,28 @@
+import {updateCartQuantity, showAddedMessage} from "../scripts/amazon.js";
+
 export const cart = []
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const quantity = Number(quantitySelector.value);
+
+    if (matchingItem) {
+        matchingItem.quantity += quantity;
+    } else {
+        cart.push({
+            productId,
+            quantity
+        });
+    }
+
+    updateCartQuantity();
+    showAddedMessage(productId);
+}
